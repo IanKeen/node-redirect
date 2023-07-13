72818a66faf318f13ab0d5f5bfb289989a69d9c6
Ian Keen <iankeen82@gmail.com>
2023-07-12 19:48:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,32 +6,9 @@ app.get('/slack', (req, res) => {
   const destination = `auth-swift://oauth-callback${req.originalUrl}`;
   console.log(`redirecting ${req} to ${destination}`);
 
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <meta http-equiv="refresh" content="0; url=${destination}">
-    </head>
-
-    <body>
-        <div>
-            Taking you back to app...
-        </div>
-        <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(function() {
-            setTimeout(function() {
-              window.location = "${destination}";
-            }, 200);
-        });
-        </script>
-    </body>
-</html>
-  `;
-
+  const html = `hello`;
   res.set('Content-type','text/html')
   res.send(html);
-  // res.redirect("auth-swift://oauth-callback" + req.originalUrl);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
