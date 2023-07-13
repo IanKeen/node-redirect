2f54c357b76417cfa57b8ce60d5288501d7addba
Ian Keen <iankeen82@gmail.com>
2023-07-12 20:03:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,26 @@ app.get('/slack', (req, res) => {
   const destination = 'auth-swift://oauth-callback' + req.originalUrl;
   console.log(`redirecting ${req} to ${destination}`);
 
-  const html = 'hello';
+  const html = '<!DOCTYPE html>' +
+                '<html lang="en">' +
+                '    <head>' +
+                '      <meta http-equiv="refresh" content="0; url=' + destination + '">' +
+                '    </head>' +
+                '    <body>' +
+                '        <div>' +
+                '            Taking you back to app...' +
+                '        </div>' +
+                '        <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>' +
+                '        <script type="text/javascript">' +
+                '        $(function() {' +
+                '            setTimeout(function() {' +
+                '              window.location = "' + destination + '";' +
+                '            }, 200);' +
+                '        });' +
+                '        </script>' +
+                '    </body>' +
+                '</html>';
+
   res.send(html);
 });
 
