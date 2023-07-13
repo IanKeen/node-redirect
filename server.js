61aa66494b349ba7599f9eb8d21f0f5e1340fd5d
Ian Keen <iankeen82@gmail.com>
2023-07-12 19:55:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/slack', (req, res) => {
-  const destination = `auth-swift://oauth-callback${req.originalUrl}`;
+  const destination = 'auth-swift://oauth-callback' + req.originalUrl;
   console.log(`redirecting ${req} to ${destination}`);
 
-  const html = `hello`;
-  res.set('Content-type','text/html')
+  const html = 'hello';
   res.send(html);
 });
 
