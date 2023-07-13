836f676b17e04501a18c9764c6d28be92e8b3340
Ian Keen <iankeen82@gmail.com>
2023-07-13 09:40:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/slack', (req, res) => {
-  const destination = 'auth-swift://oauth-callback' + req.originalUrl;
-  console.log(`redirecting ${req} to ${destination}`);
+    const destination = 'auth-swift://oauth-callback' + req.originalUrl;
+    console.log(`redirecting ${req} to ${destination}`);
 
-  const html = '<!DOCTYPE html>' +
-                '<html lang="en">' +
-                '    <head>' +
-                '      <meta http-equiv="refresh" content="0; url=' + destination + '">' +
-                '    </head>' +
-                '    <body>' +
-                '        <div>' +
-                '            Taking you back to app...' +
-                '        </div>' +
-                '        <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>' +
-                '        <script type="text/javascript">' +
-                '        $(function() {' +
-                '            setTimeout(function() {' +
-                '              window.location = "' + destination + '";' +
-                '            }, 200);' +
-                '        });' +
-                '        </script>' +
-                '    </body>' +
-                '</html>';
+    const html = '<!DOCTYPE html>' +
+                 '<html lang="en">' +
+                 '    <head>' +
+                 '    </head>' +
+                 '    <body>' +
+                 '        <div>' +
+                 '            Taking you back to app...' +
+                 '        </div>' +
+                 '        <script type="text/javascript">' +
+                 '        window.webkit.messageHandlers.bridge.postMessage({ url: "' + destination + '" });' +
+                 '        </script>' +
+                 '    </body>' +
+                 '</html>';
 
-  res.send(html);
+    res.send(html);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
