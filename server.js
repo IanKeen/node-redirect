c6d8b343f4dfa2317e29cb46fcd2018d741a51ad
Ian Keen <iankeen82@gmail.com>
2023-07-12 18:40:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.all('*', (req, res) => {
-  console.log('redirecting ${req}');
-  res.redirect('auth-swift://oauth-callback/slack' + req.originalUrl);
+app.get('/slack', (req, res) => {
+  console.log(`redirecting ${req} to auth-swift://oauth-callback${req.originalUrl}`);
+  res.redirect("auth-swift://oauth-callback" + req.originalUrl);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
